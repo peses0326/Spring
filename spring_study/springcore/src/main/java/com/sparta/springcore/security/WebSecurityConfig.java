ce0a4e870dca61b3d3d04ccffb14c08a16f494c5
Gorani92 <peses0326@gmail.com>
2021-07-03 17:35:03 +0900

    admin forbidden 페이지 추가/ 각각 @Secured("ROLE_ADMIN")

--- spring_study/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ spring_study/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sparta.springcore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +10,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean  // 비밀번호 암호화 인코더더
-   public BCryptPasswordEncoder encodePassword() {
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
 
@@ -20,29 +21,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
-
         http.authorizeRequests()
                 // image 폴더를 login 없이 허용
                 .antMatchers("/images/**").permitAll()
                 // css 폴더를 login 없이 허용
                 .antMatchers("/css/**").permitAll()
-                // 회원 관리 URL 전부를 login 없이 허용
                 .antMatchers("/user/**").permitAll()
-                // h2-console URL 을 login 없이 허용
                 .antMatchers("/h2-console/**").permitAll()
                 // 그 외 모든 요청은 인증과정 필요
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                // url controller의 return 값 login이 properties에서 뷰 템플릿 지정한 경로의 login html로 이동함.
                 .loginPage("/user/login")
-                .failureUrl("/user/login/error")  // 로그인이 실패했을 때 해당 url 위치 지정
+                .loginProcessingUrl("/user/login")
                 .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
-
                 .logout()
                 .logoutUrl("/user/logout")
-                .permitAll();
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/user/forbidden");
     }
 }
